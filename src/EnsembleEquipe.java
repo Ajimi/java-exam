538a2ed999312bc733a9b51eb4303cdef95a236b
ajimi <sloumma.21@gmail.com>
2017-12-28 14:16:14 +0100

    Add EnsembleEquipe class

---
+++ src/EnsembleEquipe.java
@@ -0,0 +1,31 @@
+import java.util.Comparator;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class EnsembleEquipe {
+    public Set<Equipe> equipes;
+
+    public Set<Equipe> getEquipes() {
+        return equipes;
+    }
+
+    public void ajouterEquipe(Equipe e) {
+        equipes.add(e);
+    }
+
+    public void supprimerEquipe(Equipe e) {
+        equipes.remove(e);
+    }
+
+    public boolean rechercherEquipe(Equipe e) {
+        return equipes.contains(e);
+    }
+
+    public void afficherEquipes() {
+        equipes.stream().forEach(System.out::println);
+    }
+
+    public Set<Equipe> trierEquipesParNom() {
+        return equipes.stream().sorted(Comparator.comparing(Equipe::getNom)).collect(Collectors.toSet());
+    }
+}
